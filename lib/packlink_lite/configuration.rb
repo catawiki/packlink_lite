da9b1adc10232730f98415784b18457906a000e1
alex quiterio <a.quiterio@catawiki.nl>
2017-12-07 16:14:08 +0100

    Move default endpoints to PacklinkLite::Configuration

--- lib/packlink_lite/configuration.rb
+++ lib/packlink_lite/configuration.rb
@@ -1,10 +1,20 @@
 module PacklinkLite
   class Configuration
     attr_accessor :api_key, :testing, :timeout
-    alias_method :testing?, :testing
+    attr_writer :api_endpoint
+
+    def api_endpoint
+      @api_endpoint || default_endpoint
+    end
 
     def timeout
       @timeout || 60
+    end
+
+    private
+
+    def default_endpoint
+      testing ? SANDBOX_URL : PRODUCTION_URL
     end
   end
 end
